51a4afdee5671117a3a51ff1ac4471c7d1c32680
Ilia Maystrenko <ilia-may@ya.ru>
2023-11-23 18:01:02 +0300

    Evaluate module debug

--- src/methods/calculate/evaluate.py
+++ src/methods/calculate/evaluate.py
@@ -6,8 +6,8 @@ from src.objects.operators.base import Operator
 
 
 def eval_addition(func: Addition) -> FloatConstant | Addition:
-    addend1 = eval(func.addend1)
-    addend2 = eval(func.addend2)
+    addend1 = eval_expression(func.addend1)
+    addend2 = eval_expression(func.addend2)
     
     if isinstance(addend1, FloatConstant) and isinstance(addend2, FloatConstant):
         return FloatConstant(value=addend1.value+addend2.value)
@@ -18,7 +18,7 @@ def eval_addition(func: Addition) -> FloatConstant | Addition:
         )
 
 
-def eval(obj: Element | Operator) -> Element | Operator:
+def eval_expression(obj: Element | Operator) -> Element | Operator:
     if isinstance(obj, (Constant, ScalarVariable)): return obj
     elif isinstance(obj, Addition): return eval_addition(obj)
     else: raise NotImplementedError
